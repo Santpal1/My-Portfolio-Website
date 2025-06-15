ec6822b814a6b5ff83e206f8e1d9075215711941
Santpal <santpalkalra1@gmail.com>
2025-06-15 19:40:29 +0530

    Added Technical skills and contact me page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import Navbar from "./components/Navbar.jsx";
 import FeatureCards from "./sections/FeatureCards.jsx";
 import ExperienceSection from "./sections/ExperienceSection.jsx";
+import TechStack from "./sections/TechStack.jsx";
+import Contact from "./sections/Contact.jsx";
 
 const App = () => {
     return (
@@ -12,6 +14,8 @@ const App = () => {
             <ShowcaseSection />
             <FeatureCards />
             <ExperienceSection />
+            <TechStack />
+            <Contact />
         </>
     )
 }
